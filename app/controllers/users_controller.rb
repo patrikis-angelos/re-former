548a7b1717402ca2f327fab138b3be8d794833c9
patrick-angelos <angelosp07@gmail.com>
2021-02-08 14:56:30 +0200

    Add authentication token in form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,12 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+
+    if @user.save
+      redirect_to action: 'new'
+    else
+      render :new
+    end
   end
 end
