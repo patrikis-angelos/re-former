110de8662e45542e2e0671ff30c235324d1b025b
necmigunduz <necmigunduz@gmail.com>
2021-02-08 17:28:42 +0300

    Add linters

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    #@user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
     @user = User.new(user_params)
 
     if @user.save
@@ -29,7 +29,7 @@ class UsersController < ApplicationController
   end
 
   private
-  
+
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  resources :users, only: [:new, :create, :update, :edit]
+  resources :users, only: %i[new create update edit]
 end
